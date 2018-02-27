3dbfcac0f83124b5511f378e297dd883ce8c7565
d35k <goktughatipoglu@yandex.com>
2018-02-28 00:29:22 +0300

    Automated backup

--- site/addons/Magpie/MagpieListener.php
+++ site/addons/Magpie/MagpieListener.php
@@ -10,14 +10,14 @@ class MagpieListener extends Listener
         'magpie.finished' => 'handleBackup'
     ];
 
-    public function handleBackup($process)
+    public function handleBackup($magpie)
     {
         // Create an array of the commands that we
         // would normally issue in the console.
         $commands[] = 'git add .';
         $commands[] = 'git commit -a -m "Automated backup"';
         $commands[] = 'git push';
-
+        $process = new \Symfony\Component\Process\Process(null);
         // The implode function will add the ` && `
         // between each command so we don't have to.
         $process->setCommandLine(implode(' && ', $commands))->run();
